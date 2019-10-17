d8102b4f8db1a3763003e605cfafa36832ec50f2
HaripriyaS-dxc <55695733+HaripriyaS-dxc@users.noreply.github.com>
2019-10-17 16:23:28 +0530

    c# asmt

---
+++ Assignments/c#.net/receipt/Area/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    class Program
+    {
+        static string[] Shapes = { "Square", "Triangle", "Rectangle", "Circle" };
+
+        static void square()
+        {
+            Console.WriteLine("Enter the side");
+            int s = int.Parse(Console.ReadLine());
+            Console.WriteLine("The area is:{0}",(s*s));
+        }
+
+        static void triangle()
+        {
+            Console.WriteLine("Enter the length and height");
+            int l = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            Console.WriteLine("The area is:{0}", (0.5*l*h));
+        }
+        static void rectangle()
+        {
+            Console.WriteLine("Enter the length and breadth");
+            int l = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("The area is:{0}", (l*b));
+        }
+
+        static void circle()
+        {
+            Console.WriteLine("Enter the radius");
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine("The area is:{0}", (Math.PI*r*r));
+        }
+
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Choose from the following Shapes");
+
+            foreach (var item in Shapes)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.WriteLine();
+
+            string shape = Console.ReadLine();
+
+            switch(shape)
+            {
+                case "Square": Program.square(); break;
+                case "Triangle": Program.triangle(); break;
+                case "Rectangle": Program.rectangle(); break;
+                case "Circle": Program.circle(); break;
+
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Assignments/c#.net/receipt/receipt/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receipt
+{
+    class Program
+    {
+        static int[] serial = { 1, 2, 3, 4, 5 };
+        static string[] items = { "Notebook", "Pen", "Pencil", "Compass", "Scale" };
+        static int[] price = { 50, 20, 5, 30, 12 };
+        static int amt,c=0;
+        static int[] count= new int[30];
+        static int[] quantity=new int[30];
+        static void getAmt(int id,int qty) {
+            for (int i = 0; i < 5; i++)
+            {
+                if (id == i)
+                {
+                    amt = amt + (qty * price[i]);
+                    break;
+                }
+            }
+
+}
+static void Main(string[] args)
+        {
+            
+            Console.Write("ID\t");
+            Console.Write("Items\t\t");
+            Console.Write("Price in Rs.");
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("{0}\t{1}\t\t{2}",serial[i],items[i], price[i]);
+
+            }
+
+            int exit = 0;
+            while (exit != 1)
+            {
+            Console.WriteLine("Would you like to order? Y/N");
+            string ans = Console.ReadLine();
+            if(ans == "Y")
+            {
+               
+                    Console.WriteLine("\nSelect Item ID and Quantity");
+
+                    int id = int.Parse(Console.ReadLine());
+                    int qty = int.Parse(Console.ReadLine());
+                    
+
+                Program.getAmt(id, qty);
+
+                    count[c] = id;
+                    quantity[c] = qty;
+
+                    c += 1;
+                    
+
+                }
+                else if(ans == "N")
+            {
+                Console.WriteLine("Goodbye!");
+                 exit = 1;
+            }
+
+            }
+
+
+            Console.WriteLine("\nYour reciept\n___________________\n");
+            Console.WriteLine("Item Name\tQuantity\tPrice");
+            for (int i = 0; i < c; i++)
+            {
+                Console.WriteLine("{0}\t\t{1}\t\t{2}", items[count[i]], quantity[i], price[count[i]]);
+            }
+
+            Console.WriteLine("Your total bill amount is:"+amt);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
