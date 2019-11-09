f4e458b291bb2e4dd0d840ef37e94c628779f52a
HaripriyaS-dxc <55695733+HaripriyaS-dxc@users.noreply.github.com>
2019-11-09 14:53:48 +0530

    ef-assessment

---
+++ Assesments/EntityFramework/Assessment/Assessment/EmpDept.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment
+{
+    class EmpDept
+    {
+
+        public void DisplayAllEmp()
+        {
+            EmpDeptEntities con = new EmpDeptEntities();
+            var Departments = con.Departments;
+            var Employees = con.Employees;
+
+            foreach (Department dept in Departments)
+            {
+                Console.WriteLine($"{dept.Name}-{dept.Location}");
+                Console.WriteLine("Id\tFirst Name\tLast Name\tGender\tSalary");
+
+                foreach (var emp in Employees)
+                {
+                    if(emp.DepartmentId == dept.Id)
+                    {
+                        Console.WriteLine($"{emp.Id}\t{emp.FirstName}\t\t{emp.LastName}\t\t{emp.Gender}\t{emp.Salary}");
+                    }
+                }
+            }
+        }
+
+        public void ViewDepts()
+        {
+            EmpDeptEntities con = new EmpDeptEntities();
+            var Departments = con.Departments;
+
+            foreach (var dept in Departments)
+            {
+                Console.WriteLine($"{dept.Id} - {dept.Name}");
+            }
+        }
+        public void DisplayEmpByDept(int id)
+        {
+            EmpDeptEntities con = new EmpDeptEntities();
+            var Departments = con.Departments;
+            var Employees = con.Employees;
+
+            foreach (var dept in Departments)
+            {
+                if(dept.Id == id)
+                {
+                    Console.WriteLine($"{dept.Name}-{dept.Location}");
+                    Console.WriteLine("Id\tFirst Name\tLast Name\tGender\tSalary");
+
+                    foreach (var emp in Employees)
+                    {
+                        if(emp.DepartmentId == dept.Id)
+                        {
+                            Console.WriteLine($"{emp.Id}\t{emp.FirstName}\t\t{emp.LastName}\t\t{emp.Gender}\t{emp.Salary}");
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Assesments/EntityFramework/Assessment/Assessment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpDept call = new EmpDept();
+            bool condition = true;
+            while (condition)
+            {
+                Console.WriteLine("Select an option:\n1.Display all Employees by Department\n2.Select Department to display Employees\n3.Exit");
+                int choice1 = int.Parse(Console.ReadLine());
+                switch (choice1)
+                {
+                    case 1: call.DisplayAllEmp(); break;
+                    case 2:
+                        {
+                            Console.WriteLine("\nEnter the Department Id:");
+                            call.ViewDepts();
+                            int choice2 = int.Parse(Console.ReadLine());
+                            call.DisplayEmpByDept(choice2);
+                            break;
+                        }
+                    case 3: condition = false; break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+            }
+
+        }
+    }
+}
